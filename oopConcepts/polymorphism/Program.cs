51e4cad7ea9cee81c72df06381044dfb4ded5b51
OmarKHDR <omarsliman37@gmail.com>
2025-02-09 15:10:59 +0200

    polymorphism

---
+++ oopConcepts/polymorphism/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+* Polymorphism is directly related to inheritance
+* it means that the same method in a child can have a different 
+* implemintation than the parent method (even if they have the same name and signature)
+* - a shape may have the function GetArea, but each child may have their own implementation
+*   for getting the area
+*
+*/
